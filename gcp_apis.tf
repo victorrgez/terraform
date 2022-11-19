3487a1cc9a91bf49e99c8d0c03de8771837da5bb
victorrgez <victorrodriguezcoria@gmail.com>
2022-11-19 11:32:00 +0100

    added cloud build file for CICD

--- gcp_apis.tf
+++ gcp_apis.tf
@@ -19,6 +19,20 @@ resource "google_project_service" "artifact-registry-api" {
   disable_on_destroy = true
   disable_dependent_services = true
 }
+
+/*
+resource "google_project_service" "cloud-build-api" {
+  service = "cloudbuild.googleapis.com"
+  # Needed for automating CICD by reacting to changes in Cloud Repositories
+  timeouts {
+    create = "1m"
+    update = "1m"
+  }
+  disable_on_destroy = true
+  disable_dependent_services = true
+}
+*/
+
 /*
 resource "google_project_service" "cloud-run-api" {
   # Needed for deploying Cloud Run services
